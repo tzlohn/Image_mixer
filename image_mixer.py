d51f16f1cb2964bf8f3afb5d5433a00614e2daf1
tzlohn <57408685+tzlohn@users.noreply.github.com>
2020-03-04 19:02:09 +0100

    rename the file to remove serial number
    Not yet done, still try to figure out how to use re.

--- image_mixer.py
+++ image_mixer.py
@@ -7,7 +7,8 @@ import pyqtgraph as pg
 from PyQt5 import QtWidgets 
 import tifffile
 
-chip_size = 2080*6.5 # µm
+x_pixels = 2080
+chip_size = x_pixels*6.5 # µm
 # open folder
 # list all the file end with raw
 
@@ -16,11 +17,24 @@ root.withdraw()
 
 folderpath = filedialog.askdirectory()
 os.chdir(folderpath)
-file_list = glob.glob('*.raw')
-print(file_list)
+file_list = glob.glob('*')
+#print(file_list)
 
-pg_app = QtWidgets.QApplication(sys.argv)
-sys.exit(pg_app.exec_())
+#pg_app = QtWidgets.QApplication(sys.argv)
+
+# rename all files, remove the serial number in the end
+for aFile in file_list:
+    pattern = re.compile(r'(.raw|_meta.txt)')
+    exts = pattern.findall(aFile)    
+    ext = [exts[-1]]
+    pattern = re.compile(r'(.*)_\d+%s'%(ext))
+    new_name = pattern.findall(aFile)
+    print(new_name)
+    #os.rename(aFile,new_name[0]+'.raw')
+
+
+'''
+file_list = glob.glob('*.raw')    
 
 for aFile in file_list:
     print(aFile)
@@ -72,15 +86,24 @@ for aFile in file_list:
             dim_size = tuple(dim_size)
             with open(aFile) as p_data:
                 positive_image = np.memmap(p_data, dtype = 'uint16', mode = 'r', shape = dim_size)
-                pg.image(positive_image)
+                #pg.image(positive_image)
+
             # find out whether p_data is left or right, replace the shutterconfig in the name of n_data
-        
-            with open(aFile) as n_data:    
+            if filename_piece[1] == 'Left':
+                theOtherFile = filename_piece[0] + '_Right' + filename_piece[2] + '.raw'
+            else:
+                theOtherFile = filename_piece[0] + '_Left' + filename_piece[2] + '.raw'
+
+            with open(theOtherFile) as n_data:    
                 negative_image = np.memmap(n_data, dtype = "uint16", mode = 'r', shape = dim_size)
             # combine them     
-
+            new_image = np.zeros(dim_size)
+            new_image[:,:,0:round(x_pixels*positive_percentage)-1] = positive_image[:,:,0:round(x_pixels*positive_percentage)-1]
+            pg.image(new_image)
             # Save it as a tiff file with the same name but no shutterconfig and LZW compressed
             #imwrite(new_file_name+'.tif', data, compress=6, metadata={'axes': 'TZCYX'})  
             # delete both files     
             #os.remove(p_data)
             #os.remove(n_data)      
+sys.exit(pg_app.exec_())
+'''

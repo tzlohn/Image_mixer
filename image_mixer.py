efed5aa3076227aed09b8856453ec1a022fe879a
tzlohn <57408685+tzlohn@users.noreply.github.com>
2020-03-04 16:43:19 +0100

    Correct all re
    Now the file can identify the file name and relative values correctly

--- image_mixer.py
+++ image_mixer.py
@@ -1,9 +1,10 @@
 import numpy as np 
 import os.path
-import glob,re,shutil
+import glob,re,shutil,sys
 import tkinter as tk
 from tkinter import filedialog
 import pyqtgraph as pg
+from PyQt5 import QtWidgets 
 import tifffile
 
 chip_size = 2080*6.5 # µm
@@ -18,40 +19,40 @@ os.chdir(folderpath)
 file_list = glob.glob('*.raw')
 print(file_list)
 
-'''
-for aFile in file_list:
-    FileNameLen = len(aFile)
-    new_file_name_L = aFile((0:FileNameLen-4))+_Left+".raw"
-    new_file_name_R = aFile((0:FileNameLen-4))+_Right+".raw"
-    shutil.copyfile(aFile,new_file_name_R)
-    os.rename(aFile,new_file_name_L)
-'''    
-
+pg_app = QtWidgets.QApplication(sys.argv)
+sys.exit(pg_app.exec_())
 
 for aFile in file_list:
-    if ~os.path.exists(aFile):
+    print(aFile)
+    if not os.path.exists(aFile):
         pass
-    else    
+    else:    
         # Check whether they have x_, y_, zoom_
-        x_value = re.findall(r'%s(\d+)'%'x_',aFile)
-        zoom_value = re.findall(r'%s(\d+)'%'zoom_',aFile)
+        x_value = re.findall(r'_X((-)?\d+)',aFile)
+        zoom_value = re.findall(r'(\d+)x_',aFile)
+        
+        x_value = x_value[0]
+        x_value = int(x_value[0])
+        zoom_value = int(zoom_value[0])
         image_size = chip_size/zoom_value
-    
+
         print(x_value)
         print(zoom_value)
 
         # find out the new file name
-        pattern = re.compile(r'(\S+)(_left|_right|_Left|_Right)(\S+)%S'%('.raw'))
+        pattern = re.compile(r'(.*)(_left|_right|_Left|_Right)(.*)%s'%('.raw'))
         filename_piece = pattern.findall(aFile)
+        filename_piece = filename_piece[0]
         new_file_name = filename_piece[0]+filename_piece[2]
+        print(new_file_name)
 
         # calculate the range of image for every file
-        x_start = int(x_value) - image_size/2 
-        x_end = int(x_value) + image_size/2
+        x_start = x_value - image_size/2 
+        x_end = x_value + image_size/2
 
         # save the range in a dictionary?
         if x_start * x_end >= 0:          # if a file with a range doesn't include 0, rename the file without shutterconfig              
-
+            pass
         else:   # if a file with a range includes 0, check shutterconfig
         # calculates the percentage of the positive part and the negative part  
             positive_percentage = abs(x_end)/image_size
@@ -74,12 +75,12 @@ for aFile in file_list:
                 pg.image(positive_image)
             # find out whether p_data is left or right, replace the shutterconfig in the name of n_data
         
-            with open(aFile) as n_data    
+            with open(aFile) as n_data:    
                 negative_image = np.memmap(n_data, dtype = "uint16", mode = 'r', shape = dim_size)
             # combine them     
 
             # Save it as a tiff file with the same name but no shutterconfig and LZW compressed
-            imwrite(new_file_name+'.tif', data, compress=6, metadata={'axes': 'TZCYX'})  
+            #imwrite(new_file_name+'.tif', data, compress=6, metadata={'axes': 'TZCYX'})  
             # delete both files     
-            os.remove(p_data)
-            os.remove(n_data)      
+            #os.remove(p_data)
+            #os.remove(n_data)      

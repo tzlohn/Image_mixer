8137884e9673d1a6cb473f21884c36077934a26b
tzlohn <57408685+tzlohn@users.noreply.github.com>
2020-08-07 08:44:55 +0200

    Running version, though left and right seems to be confused.

--- xml_LR_fuser.py
+++ xml_LR_fuser.py
@@ -38,18 +38,22 @@ def finding_index_for_zero(im_file, page_num, side, offset_threshold):
             columns = columns + column
         index_of_0 = np.where(columns == 0)[0]
         size_of_0 = index_of_0.shape[0]
-    return n
+
+    if side is "left":
+        return n
+    elif side is "right":
+        return n-1
 
 def get_dim_match_image(im, x_diff,y_diff,side):
     dim_shape = im.shape
-    if side is "Left":
+    if side == "Left":
         if x_diff > 0:
             filling_zero_matrix  = np.zeros(shape = (dim_shape[0],x_diff), dtype = "uint16")
             im = np.concatenate([im ,filling_zero_matrix], axis = 1)
         if y_diff > 0:
             filling_zero_matrix  = np.zeros(shape = (y_diff,im.shape[1]), dtype = "uint16")
             im = np.concatenate([im ,filling_zero_matrix], axis = 0)            
-    elif side is "Right":
+    elif side == "Right":
         if x_diff < 0:
             filling_zero_matrix  = np.zeros(shape = (dim_shape[0],abs(x_diff)), dtype = "uint16")
             im = np.concatenate([filling_zero_matrix,im], axis = 1)
@@ -61,10 +65,29 @@ def get_dim_match_image(im, x_diff,y_diff,side):
 def remove_comma_from_string(string):
     return string[0:len(string)-1]
 
-def save2_2D(n,imfile,overlap_offset,cutting_pixel,x_diff,y_diff,side,dest_folder):    
+def find_0_pos(all_positions,image_size_x):
+    # this function tries to find out the part where the image contains 0
+    for ind,a_position in enumerate(all_positions):
+        pos = float(a_position)
+        all_positions[ind] = pos
+        if abs(pos) > 0.5* image_size_x:
+            continue
+        else:
+            break
+    return pos
+
+def save2_2D(n,imfile,overlap_offset,cutting_pixel,x_diff,y_diff,side,dest_folder,removed_x):    
     img = TFF.TiffFile(imfile)
     im = TFF.imread(imfile, key = n)
-    im = im[:,overlap_offset:cutting_pixel]
+
+    if side == "Right":
+        #im = im[:,overlap_offset:cutting_pixel]
+        im = im[:,removed_x+overlap_offset:cutting_pixel]
+    elif side == "Left":
+        size_left = img.pages[0].shape
+        #im = im[:,cutting_pixel:size_left[1] - overlap_offset]
+        im = im[:,cutting_pixel:size_left[1] - overlap_offset - removed_x]
+
     an_image = get_dim_match_image(im,x_diff,y_diff,side)
 
     page_num_str = str(len(img.pages))
@@ -75,6 +98,8 @@ def save2_2D(n,imfile,overlap_offset,cutting_pixel,x_diff,y_diff,side,dest_folde
 
     TFF.imwrite(name, an_image, bigtiff = True)
     shutil.move(name,dest_folder)
+
+    return an_image.shape
 # part 1: modify images according to meta files
 # 1) rotates both images 90 degree
 # 2) remove overlapped pixels to make both dimensions identical
@@ -128,44 +153,52 @@ def main():
         page_num_left = len(left_tif.pages)
 
     dest_folder = ["right_rot","left_rot"]
-    os.mkdir(dest_folder[0])
-    os.mkdir(dest_folder[1])
+    #os.mkdir(dest_folder[0])
+    #os.mkdir(dest_folder[1])
 
     ## magic number zone
     offset_threshold = 100
     ##
 
     ## removing 90% of overlap
-    pos = max(all_left_positions)
-    pos = float(pos)
     image_size = pixel_size_x * x_pixels
-    ratio_L_to_edge = (pos + 0.5*image_size)/image_size
-    ratio_R_to_edge = abs(pos - 0.5*image_size)/image_size
-    if ratio_L_to_edge >0.95:
+    pos = find_0_pos(all_left_positions,image_size)
+
+    # only for the current file:TL200618
+    if pos != float(all_left_positions[-1]):
+        removed_x = round(0.5*(size_left[1]-pixel_size_x))
+    else:
+        removed_x = 0
+
+    ratio_of_Right = abs(pos + 0.5*image_size)/image_size 
+    ratio_of_Left = abs(pos - 0.5*image_size)/image_size
+    if ratio_of_Right >0.95:
+        # if one side has the ratio larger than 95%, the the other side will be totally discarded.
         overlap_offset_L = 0
-    else:
-        overlap_offset_L = round((1-(ratio_L_to_edge+0.05))*image_size)
-
-    if ratio_R_to_edge >0.95:
+        overlap_offset_R = x_pixels
+    elif ratio_of_Left >0.95:
         overlap_offset_R = 0
+        overlap_offset_L = x_pixels
     else:
-        overlap_offset_R = round((1-(ratio_R_to_edge+0.05))*image_size)
-
-
+        # 10% overlap
+        overlap_offset_L = round((ratio_of_Left+0.05)*x_pixels)
+        overlap_offset_R = round((ratio_of_Right+0.05)*x_pixels)
+        
     # finding upper and lower 0 lines
     left_cutting_pixel = finding_index_for_zero(left_file, page_num_left, "left", offset_threshold) # should be a number closer to 0
     right_cutting_pixel = finding_index_for_zero(right_file, page_num_right, "right", offset_threshold) # should be a number closer to size_right
     y_diff = size_right[0]-size_left[0]
-    x_diff = (right_cutting_pixel+1-overlap_offset_R)-(size_left[1]- overlap_offset_L - left_cutting_pixel)
-
+    x_diff = (right_cutting_pixel-overlap_offset_R)-(size_left[1]- overlap_offset_L - left_cutting_pixel)
+    
     core_no = multiprocessing.cpu_count()-1
-    Pool_input_right = [(n,right_file,overlap_offset_R,right_cutting_pixel,x_diff,y_diff,"right",dest_folder[0]) for n in range(0,page_num_right)]
-    Pool_input_left = [(n,left_file,overlap_offset_L,left_cutting_pixel,x_diff,y_diff,"left",dest_folder[1]) for n in range(0,page_num_left)]
+    print(core_no)
+    Pool_input_right = [(n,right_file,overlap_offset_R,right_cutting_pixel,x_diff,y_diff,"Right",dest_folder[0],removed_x) for n in range(page_num_right)]
+    Pool_input_left = [(n,left_file,overlap_offset_L,left_cutting_pixel,x_diff,y_diff,"Left",dest_folder[1],removed_x) for n in range(page_num_left)]
     Pool = multiprocessing.Pool(processes= core_no)
-    result = Pool.starmap(save2_2D,Pool_input_right)
-    result = Pool.starmap(save2_2D,Pool_input_left)
+    im_shape = Pool.starmap(save2_2D,Pool_input_right)
+    im_shape = Pool.starmap(save2_2D,Pool_input_left)
     Pool.close()
- 
+
     t_end = time.time()
     print("%.2f"%(t_end-t_start))
         
@@ -191,7 +224,7 @@ def main():
     dim_H = pixel_size_x
     dim_D = z_stepsize
 
-    xml_name = "terastitcher.xml"
+    xml_name = "terastitcher_for_LR.xml"
 
     slice_no = [len(right_tif.pages),len(left_tif.pages)]
     offset_H = 0
@@ -202,11 +235,7 @@ def main():
     else:
         offset_V = 0 
 
-    stack_rows = 2
-    stack_column = 1
-
-    image_files = ["Right_rot.tif","Left_rot.tif"]
-    shift_no = [1, an_image.shape[0]-x_pixels]
+    shift_no = [1, im_shape[0][0]-0.1*x_pixels]
 
     with open(xml_name,'w') as xml_file:
         xml_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n")
@@ -220,7 +249,7 @@ def main():
         xml_file.write("    <dimensions stack_rows=\"%d\" stack_columns=\"%d\" stack_slices=\"%d\" />\n"%(2,1,max(slice_no)))
         xml_file.write("    <STACKS>\n")
         
-        for n in range(len(image_files)):
+        for n in range(len(dest_folder)):
             xml_file.write("        <Stack N_CHANS=\"1\"")
             xml_file.write(" N_BYTESxCHAN=\"%d\""%(bit/8))
                 
